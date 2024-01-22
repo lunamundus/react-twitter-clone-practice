8d37ddb4b0dcb090dad223219e2dac05bc790b6a
Byeongkuk Oh <lunamundus1007@gmail.com>
2024-01-22 22:39:18 +0900

    #2.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNssDMHzvnvrHLx4TyFxkVagyrrgG5Slk",
+  authDomain: "react-twitter-clone-practice.firebaseapp.com",
+  projectId: "react-twitter-clone-practice",
+  storageBucket: "react-twitter-clone-practice.appspot.com",
+  messagingSenderId: "802155933543",
+  appId: "1:802155933543:web:b45e5b2a6b0cbacc8089c2",
+};
+
+const app = initializeApp(firebaseConfig);
